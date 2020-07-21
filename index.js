5a0e8e3471c9982753026b62972a1e73b99ee11f
Tobias Tan <tobiast@monark.com>
2020-07-21 14:44:05 -0700

    Initial data and object

---
+++ index.js
@@ -0,0 +1,31 @@
+const data = require('data')
+
+const sanitize = (input) => {
+  return String(input).toUpperCase().replace(/[^A-Z0-9 \-]/g,'').substr(0,15)
+}
+
+const defaultData = {
+	countryCode: undefined,
+	postalCodeType: 1,
+	pattern: /^([0-9A-Z\-\s]?){15}$/,
+	placeholder: 'Postal code'
+}
+
+const get = (countryCode) => {
+  let matchCountry = data.filter(c=>c.countryCode===countryCode)
+  return matchCountry.length ? matchCountry[0] : defaultData
+}
+
+const validate = (input, countryCode, sanitize) => {
+  let testedValue = sanitize ? sanitize(input) : String(input)
+  let countryData = get(countryCode)
+  return countryData.pattern.test(testedValue)
+}
+
+module.exports = {
+  data,
+  sanitize,
+  get,
+  defaultData,
+  validate
+}
